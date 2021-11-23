93a24ba8194ed1af983cc45515fd967b76f8c50c
Jason Jacobs <nerfnerd@gmail.com>
2021-11-23 15:27:56 -0800

    Make it so we can see all the stories.

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -37,8 +37,10 @@ const BlogPage = ({ blok }) => {
   const featuredPosts = blok.posts.filter(post => post.content && post.content.featured && post.content.featured == true)
   const morePosts = blok.posts.filter(post => post.content && post.content.featured != null && post.content.featured == false)
   const moreWidth = windowDimensions.width * morePosts.length
-
-
+console.log("*********");
+console.log(featuredPosts);
+  console.log("*********");
+  console.log(morePosts);
   for (const post of morePosts)
   {
     if(post.content.created_at == "") {
@@ -58,43 +60,43 @@ const BlogPage = ({ blok }) => {
   return (
     <SbEditable content={blok} key={blok._uid}>
       <div className='page-container page-blogs'>
-        {view === 'main' && (
-          <>
-            <div
-              className='section section-black section-column page-blogs-top'
-              style={{ 
-                backgroundImage: `url(${imgMask3})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                backgroundSize: 'contain'
-              }}
-            >
-              <h1>Blog</h1>
-            </div>
-            <button
-              className='page-blogs-latestbutton'
-              onClick={(e) => {
-                setView('list');
-              }}
-            >
-              <span>The Latest</span>
-              <img src={imgChevronRight} />
-            </button>
-            {featuredPosts.length > 0 && (
-              <div className='section section-white section-column page-blogs-latest'>
-                <img src={featuredPosts[0].content.image} className='page-blogs-latest-img' />
-                <span className='page-blogs-latest-date'>
-                  {featuredPosts[0].published_at}
-                </span>
-                <h2 className='page-blogs-latest-title'>
-                  {featuredPosts[0].content.title}
-                </h2>
-                <p className='page-blogs-latest-intro'>{featuredPosts[0].content.intro}</p>
-              </div>
-            )}
-          </>
-        )}
-        {view === 'list' && (
+        {/*{view === 'main' && (*/}
+        {/*  <>*/}
+        {/*    <div*/}
+        {/*      className='section section-black section-column page-blogs-top'*/}
+        {/*      style={{*/}
+        {/*        backgroundImage: `url(${imgMask3})`,*/}
+        {/*        backgroundRepeat: 'no-repeat',*/}
+        {/*        backgroundPosition: 'center center',*/}
+        {/*        backgroundSize: 'contain'*/}
+        {/*      }}*/}
+        {/*    >*/}
+        {/*      <h1>Blog</h1>*/}
+        {/*    </div>*/}
+        {/*    <button*/}
+        {/*      className='page-blogs-latestbutton'*/}
+        {/*      onClick={(e) => {*/}
+        {/*        setView('list');*/}
+        {/*      }}*/}
+        {/*    >*/}
+        {/*      <span>The Latest</span>*/}
+        {/*      <img src={imgChevronRight} />*/}
+        {/*    </button>*/}
+        {/*    {featuredPosts.length > 0 && (*/}
+        {/*      <div className='section section-white section-column page-blogs-latest'>*/}
+        {/*        <img src={featuredPosts[0].content.image} className='page-blogs-latest-img' />*/}
+        {/*        <span className='page-blogs-latest-date'>*/}
+        {/*          {featuredPosts[0].published_at}*/}
+        {/*        </span>*/}
+        {/*        <h2 className='page-blogs-latest-title'>*/}
+        {/*          {featuredPosts[0].content.title}*/}
+        {/*        </h2>*/}
+        {/*        <p className='page-blogs-latest-intro'>{featuredPosts[0].content.intro}</p>*/}
+        {/*      </div>*/}
+        {/*    )}*/}
+        {/*  </>*/}
+        {/*)}*/}
+        {/*{view === 'list' && (*/}
           <>
             <div className='section section-white section-column page-blogs-list'>
               <button
@@ -107,7 +109,7 @@ const BlogPage = ({ blok }) => {
               </button>
               <h1>The Latest</h1>
               <div className='blog-list'>
-                {morePosts.reverse().map((post, index) => (
+                {featuredPosts.reverse().map((post, index) => (
                   <div className='blog-list-item' key={`blog-item-${index}`}>
                     <div className='blog-list-item-left'>
                       <img src={post.content.image} />
@@ -123,7 +125,7 @@ const BlogPage = ({ blok }) => {
               </div>
             </div>
           </>
-        )}
+
       </div>
     </SbEditable>
   )
