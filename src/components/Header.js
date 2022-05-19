9dbace7a2b16418a1f625f83038e61564dbc0b47
wdev733 <wdev733@gmail.com>
2022-05-19 11:44:28 -0400

    update community link on the header for ccc page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,136 +1,169 @@
-import React, { useState, useEffect } from 'react'
-import Link from 'gatsby-link'
-import {isMobileOnly} from 'react-device-detect'
+import React, { useState, useEffect } from "react"
+import Link from "gatsby-link"
+import { isMobileOnly } from "react-device-detect"
 
-import logoBlueBlack from '@images/logo-blue-black.png';
+import logoBlueBlack from "@images/logo-blue-black.png"
 
-import { featureSubMenus, eventSubMenus } from '@helpers/data'
+import { featureSubMenus, eventSubMenus } from "@helpers/data"
 
-import cn from 'classnames';
+import cn from "classnames"
 
-const featureMenuKeys = featureSubMenus.map((menu) => menu.key);
+const featureMenuKeys = featureSubMenus.map(menu => menu.key)
 
 const Header = ({ pathname }) => {
- 
-  const [showMenu, setShowMenu] = React.useState(isMobileOnly ? false : true);
-  const [menu, setMenu] = useState('about-us');
-  
-  const [featureMenuOpen, setFeatureMenuOpen] = useState(false);
-  const [eventMenuOpen, setEventMenuOpen] = useState(false);
+  const [showMenu, setShowMenu] = React.useState(isMobileOnly ? false : true)
+  const [menu, setMenu] = useState("about-us")
+
+  const [featureMenuOpen, setFeatureMenuOpen] = useState(false)
+  const [eventMenuOpen, setEventMenuOpen] = useState(false)
+
+  const [communityLink, setCommunityLink] = useState(
+    "https://discord.gg/Ebh6YjMShu"
+  )
 
   useEffect(() => {
-    let url = '';
+    let url = ""
     if (typeof window !== `undefined`) {
-      url = window.location.href;
-      console.log(url);
+      url = window.location.href
+      console.log(url)
     }
 
     for (const item of featureSubMenus) {
       if (url.includes(`features/${item.key}`)) {
-        setMenu(item.key);
-        setFeatureMenuOpen(true);
-        break;
+        setMenu(item.key)
+        setFeatureMenuOpen(true)
+        break
       }
     }
 
     for (const item of eventSubMenus) {
       if (url.includes(`events/${item.key}`)) {
-        setMenu(item.key);
-        setEventMenuOpen(true);
-        break;
+        setMenu(item.key)
+        setEventMenuOpen(true)
+        break
       }
     }
 
-    if (url.includes('careers')) {
-      setMenu('careers');
+    if (url.includes("careers")) {
+      setMenu("careers")
     }
-    if (url.includes('about-us')) {
-      setMenu('about-us');
+    if (url.includes("about-us")) {
+      setMenu("about-us")
     }
-    if (url.includes('blog')) {
-      setMenu('blog');
+    if (url.includes("blog")) {
+      setMenu("blog")
     }
-    if (url.includes('cross-chain-coalition')) {
-      setMenu('cross-chain-coalition')
+    if (url.includes("cross-chain-coalition")) {
+      setMenu("cross-chain-coalition")
+      setCommunityLink(
+        "https://docs.google.com/forms/d/1fr6EHck8hY8z4RgAlCdRbrqWFdVz-8a6C_YNWSLMTWs/edit"
+      )
     }
-
-  }, []);
+  }, [])
 
   return (
     <header>
-      <div className='header-menu'>
+      <div className="header-menu">
         <div
-          className='header-menu-mobilebutton'
-          onClick={(e) => { setShowMenu(!showMenu) }}
+          className="header-menu-mobilebutton"
+          onClick={e => {
+            setShowMenu(!showMenu)
+          }}
         >
           <img src={logoBlueBlack} />
         </div>
         {showMenu && (
-          <div className='header-menu-mobile'>
-            <div className='header-menu-mobile-mask' onClick={(e) => { setShowMenu(false) }}>
-            </div>
-            <div className='header-menu-mobile-panel'>
+          <div className="header-menu-mobile">
+            <div
+              className="header-menu-mobile-mask"
+              onClick={e => {
+                setShowMenu(false)
+              }}
+            ></div>
+            <div className="header-menu-mobile-panel">
               <Link
-                className={cn('header-menu-mobile-item black', { active: featureMenuKeys.includes(menu) || featureMenuOpen })}
+                className={cn("header-menu-mobile-item black", {
+                  active: featureMenuKeys.includes(menu) || featureMenuOpen,
+                })}
                 to="/features/blockchain-protocols"
               >
                 {`Features`}
               </Link>
               {featureMenuOpen && (
-                <ul className='sub-nav-mobile-menu'>
-                  {featureSubMenus.map((item) => (
+                <ul className="sub-nav-mobile-menu">
+                  {featureSubMenus.map(item => (
                     <Link
                       key={`features-mobile-submenu-${item.key}`}
-                      className={cn('header-menu-mobile-item', `${item.color}`, { active: menu === item.key })}
+                      className={cn(
+                        "header-menu-mobile-item",
+                        `${item.color}`,
+                        { active: menu === item.key }
+                      )}
                       to={item.link}
                     >
-                    {item.menu}
-                  </Link>
+                      {item.menu}
+                    </Link>
                   ))}
                 </ul>
               )}
-              <Link 
-                className={cn('header-menu-mobile-item', { active: menu === 'careers' })}
+              <Link
+                className={cn("header-menu-mobile-item", {
+                  active: menu === "careers",
+                })}
                 to={`/careers/`}
               >
                 {`Careers`}
               </Link>
               <Link
-                className={cn('header-menu-mobile-item blue', { active: eventSubMenus.includes(menu) || eventMenuOpen })}
+                className={cn("header-menu-mobile-item blue", {
+                  active: eventSubMenus.includes(menu) || eventMenuOpen,
+                })}
                 to="/events/upcoming-events"
               >
                 {`Events`}
               </Link>
               {eventMenuOpen && (
-                <ul className='sub-nav-mobile-menu'>
-                  {eventSubMenus.map((item) => (
+                <ul className="sub-nav-mobile-menu">
+                  {eventSubMenus.map(item => (
                     <Link
                       key={`event-mobile-submenu-${item.key}`}
-                      className={cn('header-menu-mobile-item', { active: menu === item.key })}
+                      className={cn("header-menu-mobile-item", {
+                        active: menu === item.key,
+                      })}
                       to={item.link}
                     >
-                    {item.menu}
-                  </Link>
+                      {item.menu}
+                    </Link>
                   ))}
                 </ul>
               )}
-              <Link 
-                className={cn('header-menu-mobile-item', { active: menu === 'about-us' })}
+              <Link
+                className={cn("header-menu-mobile-item", {
+                  active: menu === "about-us",
+                })}
                 to={`/about-us/`}
               >
                 {`About Us`}
               </Link>
-              <a href='https://discord.com/invite/Ebh6YjMShu' className='header-menu-mobile-item' target='_blank'>
+              <a
+                href="https://discord.com/invite/Ebh6YjMShu"
+                className="header-menu-mobile-item"
+                target="_blank"
+              >
                 Community
               </a>
-              <Link 
-                className={cn('header-menu-mobile-item', { active: menu === 'blog' })}
+              <Link
+                className={cn("header-menu-mobile-item", {
+                  active: menu === "blog",
+                })}
                 to={`/cross-chain-coalition/`}
               >
                 {`CCC`}
               </Link>
-              <Link 
-                className={cn('header-menu-mobile-item', { active: menu === 'blog' })}
+              <Link
+                className={cn("header-menu-mobile-item", {
+                  active: menu === "blog",
+                })}
                 to={`/blog/`}
               >
                 {`Blog`}
@@ -138,12 +171,12 @@ const Header = ({ pathname }) => {
             </div>
           </div>
         )}
-        <a href='https://discord.gg/Ebh6YjMShu' target='_blank' className='header-menu-item'>
+        <a href={communityLink} target="_blank" className="header-menu-item">
           {`JOIN THE COMMUNITY`}
         </a>
       </div>
     </header>
   )
-};
+}
 
-export default Header;
+export default Header
